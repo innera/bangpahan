837977817daa6e7070f566bbdf85f5f175b252e4
innera <takhj7@hotmail.com>
2017-11-17 10:51:12 +0700

    17/11/2560

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { TestComponent } from './test/test.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+   declarations: [    
+    FooterComponent,
+    AppComponent,
+    TestComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+   
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
